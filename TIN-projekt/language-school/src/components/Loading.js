11cf8a45f83b12e352aeefe5eae1015e067fd323
Krzysztof <krzysztof.drapson@gmail.com>
2025-01-12 02:58:06 +0100

    Add error styling to Loading component

--- TIN-projekt/language-school/src/components/Loading.js
+++ TIN-projekt/language-school/src/components/Loading.js
@@ -3,7 +3,11 @@ import { useTranslation } from "react-i18next";
 
 function Loading({ error }) {
   const { t } = useTranslation();
-  return <div>{error ? <p>{error}</p> : <p>{t("loading")}...</p>}</div>;
+  return (
+    <div>
+      {error ? <p className="error">{error}</p> : <p>{t("loading")}...</p>}
+    </div>
+  );
 }
 
 export default Loading;
